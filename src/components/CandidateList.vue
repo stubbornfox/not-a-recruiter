fffe1416e6eb00540d23d52c7053fc97d3872f94
Ha Le <nhiha001@gmail.com>
2022-10-18 23:32:44 +0700

    Initial commit

---
+++ src/components/CandidateList.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import {ref, onMounted} from 'vue'
+  import axios from 'axios'
+
+  const candidates = ref([])
+  let error = ref([])
+
+  onMounted(() => {
+    axios.get('http://localhost:3000/candidates')
+    .then((response) => {
+      candidates.value = response.data;
+    })
+   .catch((e) => {
+      error.value.push(e);
+    })
+  })
+</script>
+
+
+<template>
+ <div id="candidates">
+  <div v-for="(candidate, index) in candidates" :key="candidate.id">
+   <p>{{candidate.first_name}}</p>
+  </div>
+ </div>
+</template>
+
