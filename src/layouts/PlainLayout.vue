36818efc0bbe99fe4c1dc917e199bf2bc13eec98
Ha Le <nhiha001@gmail.com>
2022-11-13 02:23:17 +0700

    [f] Add js to plainlayout

--- src/layouts/PlainLayout.vue
+++ src/layouts/PlainLayout.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex items-center justify-center h-full">
     <slot />
+    {{route.meta}}
   </div>
 </template>
 
 <script setup>
   import { useRoute } from 'vue-router'
   const route = useRoute()
-  console.log(route.meta)
 </script>
