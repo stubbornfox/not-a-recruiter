4c0997afb050995690257fffb198093c3c78987b
Ha Le <nhiha001@gmail.com>
2023-02-12 12:58:35 +0700

    [f] Applicant Box

--- src/components/Pipeline.vue
+++ src/components/Pipeline.vue
@@ -9,9 +9,35 @@
       </div>
     </div>
     <div class="mt-3">
-      <div v-for="applicant in applicants" class="boder border-neutrals-20">
-        {{applicant.name}}
+      <div v-if="applicants.length > 0" class="flex flex-col gap-2">
+      <div v-for="applicant in applicants" class="border border-neutrals-20 lg:p-6 ">
+        <div class="flex">
+          <div>
+            <div class="font-bold text-neutrals-100">{{applicant.name}}</div>
+            <RouterLink :to="{}" class="font-semibold text-primary text-sm">View profile</RouterLink>
+          </div>
+        </div>
+        <div class="flex justify-between mt-6">
+          <div class="flex flex-col">
+            <label class="text-neutrals-60 text-sm">Applied on</label>
+            <time class="font-medium text-sm text-neutrals-100 font-semibold" :datetime="applicant.created_at" format="YYYY MM DD">
+              {{ $filters.formatDate(applicant.created_at) }}
+            </time>
+          </div>
+         <div class="flex flex-col">
+            <label class="text-neutrals-60 text-sm">Score</label>
+            <span class="font-medium text-sm text-neutrals-100 font-semibold">
+              {{ applicant.score }}
+            </span>
+          </div>
+        </div>
       </div>
+    </div>
+    <div v-else>
+      <div class="border border-neutrals-20 lg:p-6">
+        No applicants in this stage
+      </div>
+    </div>
     </div>
   </div>
 </template>
